472c9b0f6f04ece10b7e68facf642d70013bd892
rolanmed <rolanmed@gmail.com>
2019-11-20 18:42:18 -0600

    Subida de proyecto

---
+++ demoOperaciones/src/modelo/Datos.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modelo;
+
+/**
+ *
+ * @author Rolando
+ */
+public class Datos {
+    private double numA;
+    private double numB;
+
+    public Datos() {
+        this.numA = 0.0;
+        this.numB = 0.0;
+    }
+
+    public double getNumA() {
+        return numA;
+    }
+
+    public void setNumA(double numA) {
+        this.numA = numA;
+    }
+
+    public double getNumB() {
+        return numB;
+    }
+
+    public void setNumB(double numB) {
+        this.numB = numB;
+    }
+    
+    
+    
+    
+}
---
+++ demoOperaciones/src/modelo/Procesos.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modelo;
+
+/**
+ *
+ * @author Rolando
+ */
+public class Procesos {
+    private double resultado;
+
+    public Procesos() {
+        resultado = 0.0;
+    }
+    
+    public double suma(Datos d){
+        resultado = d.getNumA() + d.getNumB();
+        return resultado;
+    }
+    
+    
+    
+}
